16c284b14430827f0a837b4fd432148c5ec60356
Anjali Rathore <anjalirathore0239@gmail.com>
2024-03-21 15:39:43 +0530

    snackbar context value

--- src/context/snackbarProvider/context.tsx
+++ src/context/snackbarProvider/context.tsx
@@ -1,11 +1,32 @@
-import { PropsWithChildren, createContext } from 'react';
+import { type PropsWithChildren, createContext } from 'react';
 
 export const SnackbarContext = createContext({});
 
-function SnackBarProvider({ children }: PropsWithChildren) {
+interface SnackbarOptions {
+  maxSnackbar: number;
+  duration: string;
+  preventDuplicate: boolean;
+  position: string;
+  transition: string;
+}
+interface SnackbarProviderProps extends SnackbarOptions, PropsWithChildren {}
+const snackbarValue: SnackbarOptions = {
+  maxSnackbar: 4,
+  duration: '5000',
+  preventDuplicate: false,
+  position: 'bottom-right',
+  transition: '',
+};
+
+function SnackBarProvider({
+  children,
+  ...rest
+}: SnackbarProviderProps): JSX.Element {
   return (
-    <SnackbarContext.Provider value={}>
+    <SnackbarContext.Provider value={{ ...snackbarValue, ...rest }}>
       {children}
     </SnackbarContext.Provider>
   );
 }
+
+export default SnackBarProvider;
